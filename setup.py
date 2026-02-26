bfa7458c567b8bfc30958a96c326ab810108d36e
Riyan <thevaluables04@gmail.com>
2026-02-26 09:29:44 +0530

    made setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open('requirements.txt') as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+setup(
+    name="End to End ML PRO",
+    author="Riyan",
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+    
+)
