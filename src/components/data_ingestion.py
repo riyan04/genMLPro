0c4f749a3e2f3a36727a4193c124ca04bbee2f3f
Riyan <thevaluables04@gmail.com>
2026-02-27 23:01:22 +0530

    Data Transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,9 @@
 import os
 import sys
+
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
 
 import pandas as pd
 
@@ -45,4 +47,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_path=train_data, test_path=test_data)
